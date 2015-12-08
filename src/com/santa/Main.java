648f75ba9376fd0d70b856fd16c1c288348957f7
Neal Haggard <Neal.Haggard@sas.com>
2015-12-08 13:37:11 -0500

    Updated notifyBasement to be a stream based solution.  Still not efficient as I'm not remembering the previous solutions as I move along.

--- src/com/santa/Main.java
+++ src/com/santa/Main.java
@@ -2,9 +2,13 @@ package com.santa;
 
 import com.santa.day1.ElevatorParser;
 
+/**
+ * Test the output
+ */
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(ElevatorParser.parse(Integer.parseInt(args[0]), args[1]));
+        System.out.println("New Floor: " + ElevatorParser.parse(Integer.parseInt(args[0]), args[1]));
+        System.out.println("Basement on Instruction: " + ElevatorParser.notifyBasement(Integer.parseInt(args[0]), args[1]));
     }
 }
--- src/com/santa/day1/ElevatorParser.java
+++ src/com/santa/day1/ElevatorParser.java
@@ -1,41 +1,47 @@
 package com.santa.day1;
 
-import java.util.Arrays;
+import java.util.stream.IntStream;
 
 /**
- * TODO: Put a good comment here
+ * --- Day 1: Not Quite Lisp ---
+ * <p>
+ * Santa is trying to deliver presents in a large apartment building, but he can't find the right floor
+ * - the directions he got are a little confusing. He starts on the ground floor (floor 0) and then follows the
+ * instructions one character at a time.
+ * <p>
+ * An opening parenthesis, (, means he should go up one floor,
+ * and a closing parenthesis, ), means he should go down one floor.
+ * <p>
+ * The apartment building is very tall, and the basement is very deep; he will never find the top or bottom floors.
+ * <p>
+ * For example:
+ * <p>
+ * (()) and ()() both result in floor 0.
+ * ((( and (()(()( both result in floor 3.
+ * ))((((( also results in floor 3.
+ * ()) and ))( both result in floor -1 (the first basement level).
+ * ))) and )())()) both result in floor -3.
  *
- * @author Neal Haggard (nehagg)
- * @since 4.17
+ * @author Neal Haggard
  */
 public class ElevatorParser {
-    public static int parse( int base, String instructions) {
+    public static int parse(int base, String instructions) {
         return instructions.chars().reduce(base, ElevatorParser::parseChar);
     }
 
     private static int parseChar(int base, int instruction) {
         if (instruction == ')') {
-            return --base;
+            return base - 1;
         } else if (instruction == '(') {
-            return ++base;
+            return base + 1;
         }
-        System.out.println("parseChar:: base = [" + base + "], instruction = [" + instruction + "]");
+
         return base;
     }
 
     public static int notifyBasement(int base, String instructions) {
-        int level = base;
-        int index;
-
-
-        for (index = 0; index < instructions.length() && level >= 0; index++) {
-            level = parseChar(level, instructions.charAt(index));
-        }
-
-        if (level >= 0) {
-            return -1;
-        }
-
-        return index;
+        return IntStream.range(0, instructions.length() + 1)
+                .filter(i -> parse(base, instructions.substring(0, i)) < 0)
+                .findFirst().orElse(-1);
     }
 }

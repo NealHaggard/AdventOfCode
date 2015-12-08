bc784263d5b84f8ea53c40a2abe8ee69ea251ed2
Neal Haggard <Neal.Haggard@sas.com>
2015-12-08 13:43:29 -0500

    Cleaned up comments.

--- src/com/santa/day1/ElevatorParser.java
+++ src/com/santa/day1/ElevatorParser.java
@@ -25,23 +25,49 @@ import java.util.stream.IntStream;
  * @author Neal Haggard
  */
 public class ElevatorParser {
+    /**
+     * Parse instructions for an elevator.
+     *
+     * @param base Base floor to start at.
+     * @param instructions String holding ( or ).  ( goes up, ) goes down.
+     * @return new floor after completing instructions
+     */
     public static int parse(int base, String instructions) {
         return instructions.chars().reduce(base, ElevatorParser::parseChar);
     }
 
-    private static int parseChar(int base, int instruction) {
-        if (instruction == ')') {
-            return base - 1;
-        } else if (instruction == '(') {
-            return base + 1;
-        }
-
-        return base;
-    }
-
+    /**
+     * Notify the first time the instructions would cause the user to go into the basement (below floor 0).
+     *
+     * @param base Base floor to start at.
+     * @param instructions String holding ( or ).  ( goes up, ) goes down.
+     * @return index in the instruction that the level would be in the basement
+     */
     public static int notifyBasement(int base, String instructions) {
-        return IntStream.range(0, instructions.length() + 1)
+        return IntStream.range(1, instructions.length() + 1)
                 .filter(i -> parse(base, instructions.substring(0, i)) < 0)
                 .findFirst().orElse(-1);
+    }
+
+    /**
+     * Helper to parse a particular instruction.
+     *
+     * ( goes up
+     * ) goes down
+     *
+     * Anything else stays on the same floor.
+     *
+     * @param level current level
+     * @param instruction instruction to parse
+     * @return new level
+     */
+    private static int parseChar(int level, int instruction) {
+        if (instruction == ')') {
+            return level - 1;
+        } else if (instruction == '(') {
+            return level + 1;
+        }
+
+        return level;
     }
 }
